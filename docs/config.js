115b636f93dec7088d27e1ef4da7a2e06ef30c4b
Sean Norwood <norwood.sean@gmail.com>
2021-12-19 11:06:48 -0600

    chore: add docs page

---
+++ docs/config.js
@@ -0,0 +1,76 @@
+const config = {
+  gatsby: {
+    pathPrefix: '/',
+    siteUrl: 'https://stiforr.github.io/DO-cicd-gitops/',
+    gaTrackingId: null,
+    trailingSlash: false,
+  },
+  header: {
+    logo: '',
+    logoLink: '',
+    title: '',
+    //   "<a href='https://hasura.io/learn/'><img class='img-responsive' src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/learn-logo.svg' alt='Learn logo' /></a>",
+    githubUrl: 'https://github.com/hasura/gatsby-gitbook-boilerplate',
+    helpUrl: '',
+    tweetText: '',
+    // social: `<li>
+    //     <a href="https://twitter.com/hasurahq" target="_blank" rel="noopener">
+    //       <div class="twitterBtn">
+    //         <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/twitter-brands-block.svg' alt={'Twitter'}/>
+    //       </div>
+    //     </a>
+    //   </li>
+    // 	<li>
+    //     <a href="https://discordapp.com/invite/hasura" target="_blank" rel="noopener">
+    //       <div class="discordBtn">
+    //         <img src='https://graphql-engine-cdn.hasura.io/learn-hasura/assets/homepage/discord-brands-block.svg' alt={'Discord'}/>
+    //       </div>
+    //     </a>
+    //   </li>`,
+    links: [{ text: '', link: '' }],
+    search: {
+      enabled: false,
+      indexName: '',
+      algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
+      algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+      algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
+    },
+  },
+  sidebar: {
+    forcedNavOrder: [],
+    collapsedNav: [],
+    links: [{ text: 'Hasura', link: 'https://hasura.io' }],
+    frontline: false,
+    ignoreIndex: true,
+    title:
+      "<a href='https://hasura.io/learn/'>graphql </a><div class='greenCircle'></div><a href='https://hasura.io/learn/graphql/react/introduction/'>react</a>",
+  },
+  siteMetadata: {
+    title: 'Digital Ocean K8s Challenge',
+    description: '',
+    ogImage: null,
+    docsLocation: 'https://github.com/hasura/gatsby-gitbook-boilerplate/tree/master/content',
+    favicon: 'https://graphql-engine-cdn.hasura.io/img/hasura_icon_black.svg',
+  },
+  pwa: {
+    enabled: false, // disabling this will also remove the existing service worker.
+    manifest: {
+      name: 'Gatsby Gitbook Starter',
+      short_name: 'GitbookStarter',
+      start_url: '/',
+      background_color: '#6b37bf',
+      theme_color: '#6b37bf',
+      display: 'standalone',
+      crossOrigin: 'use-credentials',
+      icons: [
+        {
+          src: 'src/pwa-512.png',
+          sizes: `512x512`,
+          type: `image/png`,
+        },
+      ],
+    },
+  },
+};
+
+module.exports = config;
